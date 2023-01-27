969e1874a49a738e9c04fc151b561ad2bd51a2ce
ergho <alexstenmark92@gmail.com>
2023-01-27 10:10:44 +0100

    I should change how this is setup later

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,7 +1,7 @@
-resource "proxmox_vm_qemu" "vm" {
+resource "proxmox_vm_qemu" "K8S" {
   for_each    = var.vm
   name        = each.value.name
-  target_node = var.proxmox_host
+  target_node = each.value.node
   clone       = var.template_name
   agent       = 1
   os_type     = "cloud-init"
@@ -11,12 +11,14 @@ resource "proxmox_vm_qemu" "vm" {
   memory      = each.value.mem
   scsihw      = "virtio-scsi-pci"
   bootdisk    = "scsi0"
+  onboot      = each.value.boot
+  startup     = each.value.order
 
   disk {
     size     = each.value.disk_size
-    type     = "scsi"
+    type     = each.value.disk_type
     storage  = var.storage_name
-    iothread = 1
+    iothread = 0
     ssd      = 1
     discard  = "on"
 
@@ -24,8 +26,48 @@ resource "proxmox_vm_qemu" "vm" {
 
   network {
     model  = "virtio"
-    bridge = "vmbr1"
-    tag    = 90
+    bridge = each.value.interface
+    tag    = each.value.vlan
+  }
+
+  ipconfig0  = "ip=${each.value.ip},gw=${each.value.gateway}"
+  nameserver = "192.168.90.5"
+  sshkeys    = <<EOF
+    ${var.ssh_key}
+  EOF
+
+}
+
+resource "proxmox_vm_qemu" "DNS" {
+  for_each    = var.dns
+  name        = each.value.name
+  target_node = each.value.node
+  clone       = var.template_name
+  agent       = 1
+  os_type     = "cloud-init"
+  cores       = each.value.cpu
+  sockets     = 1
+  cpu         = "host"
+  memory      = each.value.mem
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  onboot      = each.value.boot
+  startup     = each.value.order
+
+  disk {
+    size     = each.value.disk_size
+    type     = each.value.disk_type
+    storage  = var.storage_name
+    iothread = 0
+    ssd      = 1
+    discard  = "on"
+
+  }
+
+  network {
+    model  = "virtio"
+    bridge = each.value.interface
+    tag    = each.value.vlan
   }
 
   ipconfig0 = "ip=${each.value.ip},gw=${each.value.gateway}"
--- terraform/proxmox/variables.tf
+++ terraform/proxmox/variables.tf
@@ -1,38 +1,112 @@
 variable "vm" {
-  description = "Homelab Machines"
+  description = "Homelab K8s Machines"
   type        = map(any)
   default = {
-    controller = {
+    controller1 = {
+      node      = "pve"
       name      = "k8controller-1.ergho.org"
       mem       = 4096
       cpu       = 4
       ip        = "192.168.90.100/24"
       gateway   = "192.168.90.1"
       disk_size = "50G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr1"
+      vlan      = 90
+    }
+    controller2 = {
+      node      = "pve2"
+      name      = "k8controller-2.ergho.org"
+      mem       = 4096
+      cpu       = 4
+      ip        = "192.168.90.101/24"
+      gateway   = "192.168.90.1"
+      disk_size = "50G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr0"
+      vlan      = 90
     }
     k8w1 = {
+      node      = "pve"
       name      = "k8worker-1.ergho.org"
       mem       = 12288
       cpu       = 8
       ip        = "192.168.90.110/24"
       gateway   = "192.168.90.1"
       disk_size = "50G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr1"
+      vlan      = 90
+
     }
     k8w2 = {
+      node      = "pve"
       name      = "k8worker-2.ergho.org"
       mem       = 12288
       cpu       = 8
       ip        = "192.168.90.111/24"
       gateway   = "192.168.90.1"
       disk_size = "50G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr1"
+      vlan      = 90
     }
     k8w3 = {
+      node      = "pve"
       name      = "k8worker-3.ergho.org"
       mem       = 12288
       cpu       = 8
       ip        = "192.168.90.112/24"
       gateway   = "192.168.90.1"
       disk_size = "50G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr1"
+      vlan      = 90
+    }
+    k8w4 = {
+      node      = "pve2"
+      name      = "k8worker-4.ergho.org"
+      mem       = 12288
+      cpu       = 8
+      ip        = "192.168.90.113/24"
+      gateway   = "192.168.90.1"
+      disk_size = "50G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr0"
+      vlan      = 90
+    }
+  }
+}
+
+variable "dns" {
+  description = "Homelab DNS Machines"
+  type        = map(any)
+  default = {
+    controller1 = {
+      node      = "pve2"
+      name      = "dns.ergho.org"
+      mem       = 4096
+      cpu       = 4
+      ip        = "192.168.90.5/24"
+      gateway   = "192.168.90.1"
+      disk_size = "20G"
+      disk_type = "scsi"
+      boot      = true
+      order     = "order=110"
+      interface = "vmbr0"
+      vlan      = 90
     }
   }
 }
