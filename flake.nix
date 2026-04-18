29c1b200ce9dab940ffa2d2487d4a51b04708e7a
Alex <alexstenmark92@gmail.com>
2026-04-19 00:04:43 +0200

    add envoy-gateway see if it works

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Lab environment setup";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # Helper function to generate outputs for each system
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              kubectl
+              fluxcd
+              jq
+              yq
+              yamllint
+              yamlfmt
+            ];
+
+            shellHook = ''
+              echo "Homelab shell"
+              echo "Kubectl: $(kubectl --version | head -1)"
+              echo "fluxcd: $(fluxcd--version)"
+
+            '';
+          };
+        }
+      );
+
+    };
+}
